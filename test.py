0d0435ccb5ae17d92f4903b1416f3ba98055ab4d
Kyle Vasulka <kylevasulka@Kyles-MacBook-Pro.local>
2022-06-25 19:16:26 -0400

    Add testing for all other buttons

--- test.py
+++ test.py
@@ -1,10 +1,27 @@
 import RPi.GPIO as GPIO
 import time
 
+used_pins = [3,5,7,8,10,11,12]
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
 
+def readKeypad(whattoprint, input_pin, output_pin):
+    GPIO.setup(input_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.LOW)
+
+readKeypad("#", used_pins[3], used_pins[6])
 while True:
-    print(GPIO.input(8))
+    print(used_pins[3])
     time.sleep(1)
+
+
+    # readKeypad("6", used_pins[3], used_pins[1])
+    # readKeypad("9", used_pins[3], used_pins[0])
+    # readKeypad("0", used_pins[4], used_pins[6])
+    # readKeypad("*", used_pins[5], used_pins[6])
+    # readKeypad("7", used_pins[5], used_pins[0])
+    # readKeypad("4", used_pins[5], used_pins[1])
+    # readKeypad("1", used_pins[5], used_pins[2])
+    # readKeypad("2", used_pins[4], used_pins[2])
+    # readKeypad("3", used_pins[2], used_pins[3])
+    # readKeypad("5", used_pins[1], used_pins[4])
+    # readKeypad("8", used_pins[0], used_pins[4])
