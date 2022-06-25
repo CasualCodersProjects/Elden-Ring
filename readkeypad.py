535dbddc419eba5ab75f815cb58e4fb712796909
Kyle Vasulka <kylevasulka@Kyles-MacBook-Pro.local>
2022-06-25 19:48:40 -0400

    flip output to low because thats how its supposed to be

--- readkeypad.py
+++ readkeypad.py
@@ -15,13 +15,13 @@ signal.signal(signal.SIGINT, signal_handler)
 used_pins = [7,8,10,11,12,13,15]
 
 def readKeypad(printVal, inputPin, outputPin):
-    print(f'powering pin {outputPin}')
-    print(f'reading pin {inputPin}')
+    # print(f'powering pin {outputPin}')
+    # print(f'reading pin {inputPin}')
     # set_all_pins_input()
     # set output pin
     GPIO.setup(inputPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     # set other input pin
-    GPIO.setup(outputPin, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(outputPin, GPIO.OUT, initial=GPIO.LOW)
     # if input is low its pressed
 
     # print_all_pins()    
@@ -50,20 +50,20 @@ def print_all_pins():
 
 if __name__ == "__main__":
     GPIO.setmode(GPIO.BOARD)
-    setup()
+    # setup()
 
     print("starting keypad detection...")
 
     while True:
         readKeypad("#", used_pins[3], used_pins[6])
-        # readKeypad("6", used_pins[3], used_pins[1])
-        # readKeypad("9", used_pins[3], used_pins[0])
-        # readKeypad("0", used_pins[4], used_pins[6])
-        # readKeypad("*", used_pins[5], used_pins[6])
-        # readKeypad("7", used_pins[5], used_pins[0])
-        # readKeypad("4", used_pins[5], used_pins[1])
-        # readKeypad("1", used_pins[5], used_pins[2])
-        # readKeypad("2", used_pins[4], used_pins[2])
-        # readKeypad("3", used_pins[2], used_pins[3])
-        # readKeypad("5", used_pins[1], used_pins[4])
-        # readKeypad("8", used_pins[0], used_pins[4])
+        readKeypad("6", used_pins[3], used_pins[1])
+        readKeypad("9", used_pins[3], used_pins[0])
+        readKeypad("0", used_pins[4], used_pins[6])
+        readKeypad("*", used_pins[5], used_pins[6])
+        readKeypad("7", used_pins[5], used_pins[0])
+        readKeypad("4", used_pins[5], used_pins[1])
+        readKeypad("1", used_pins[5], used_pins[2])
+        readKeypad("2", used_pins[4], used_pins[2])
+        readKeypad("3", used_pins[2], used_pins[3])
+        readKeypad("5", used_pins[1], used_pins[4])
+        readKeypad("8", used_pins[0], used_pins[4])
