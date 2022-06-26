ebe7a4320c35bc2552b2678739c72e976fed54bc
Kyle Vasulka <kylevasulka@Kyles-MacBook-Pro.local>
2022-06-26 00:37:33 -0400

    test long running process

--- eldinring.py
+++ eldinring.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import Play
 from time import sleep
 from multiprocessing import Process
@@ -6,6 +7,9 @@ import os
 
 def play_sound(mp3_file):
     Play.init_sound()
+
+    # wait for queue to play
+
     if os.path.exists(mp3_file):
         try:
             Play.play_music(mp3_file)
@@ -16,17 +20,53 @@ def play_sound(mp3_file):
     else:
         print(f'no mp3 found for {mp3_file}')
 
-if __name__ == '__main__':
+def play_sound_with_queue(mp3_file,queue):
+    Play.init_sound()
 
-    # find mp3 files on usb stick
+    # wait for queue to play
+    obj = queue.get()
 
-    p = Process(target=play_sound, args=('',))
+    if os.path.exists(mp3_file):
+        try:
+            Play.play_music(mp3_file)
+            sleep(.25)
+        except Exception as e:
+            print("error playing song")
+            print(e)
+    else:
+        print(f'no mp3 found for {mp3_file}')
+
+def create_processes():
+    
+    queue = multiprocessing.Queue()
+
+    processes = []
+
+    p = Process(target=play_sound, args=('/mnt/usb/welcome.mp3',queue,))
     p.start()
+    processes.append(p)
+    for num in range(0,10):
+        proc = Process(target=play_sound, args=(f'/mnt/usb/{num}.mp3',))
+        proc.start()
+        processes.append(proc)
+    proc_hash = Process(target=play_sound, args=(f'/mnt/usb/#.mp3',))
+    proc_hash.start()
+    processes.append(proc_hash)
+    proc_star = Process(target=play_sound, args=(f'/mnt/usb/star.mp3',))
+    proc_star.start()
+    processes.append(proc_star)
+
+
+if __name__ == '__main__':
+    # find mp3 files on usb stick
       
     readkeypad.phone_setup()
     readkeypad.gpio_setup()
 
     while True:
+        queue = multiprocessing.Queue()
+        intro_process = Process(target=play_sound_with_queue, args=(f'/mnt/usb/welcome.mp3',queue))
+
         print('Program start! Welcome to the phone booth!')
 
         # Wait until handset is unplugged
@@ -34,24 +74,22 @@ if __name__ == '__main__':
             pass
         print('Phone was picked Up!')
 
-        p.kill()
-        p = Process(target=play_sound, args=('/mnt/usb/welcome.mp3',))
-        p.start()
+        queue.put("go")
+        # p = Process(target=play_sound, args=('/mnt/usb/welcome.mp3',))
+        # p.start()
 
         # start looking at keypad
         while True:
             pressed_button = readkeypad.check_keypad_pressed()
             if pressed_button is not None:
                 print(f'{pressed_button} was pressed!')
-                p.kill()
+                intro_process.kill()
                 sleep(0.25)
                 p = Process(target=play_sound, args=('/mnt/usb/' + pressed_button+'.mp3',))
                 p.start()
                 sleep(1)
             if readkeypad.check_phone_picked_up():
                 p.kill()
-                p = Process(target=play_sound, args=('',))
-                p.start()
                 break
 
 
