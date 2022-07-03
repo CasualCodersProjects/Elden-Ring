cc23e7f4d1edd0663a7c0cea6316e30f5c7129ad
Kyle Vasulka <kylevasulka@Kyles-MacBook-Pro.local>
2022-07-03 00:07:19 -0400

    fastapi routing

--- testringer.py
+++ testringer.py
@@ -1,10 +1,19 @@
+from fastapi import FastAPI
 
 import RPi.GPIO as GPIO
 from time import sleep
 ringer_pins = [13] 
 
 
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    ring()
+    return {"Hello": "World"}
+
 def ringer_setup():
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(ringer_pins[0], GPIO.OUT, initial=GPIO.LOW)
 
 def ring():
@@ -14,7 +23,9 @@ def ring():
         GPIO.output(ringer_pins[0], GPIO.LOW)
         sleep(1/40)
 
+ringer_setup()
 
-if __name__ == "__name__":
-    ringer_setup()
+
+if __name__ == "__main__":
+    
     ring()
